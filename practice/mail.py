9e350cc1a919bae195a23ba30610412806ebefae
Nathan attoh <nnaattoh@st.ug.edu.gh>
2025-09-30 12:07:48 +0000

    config email

--- practice/mail.py
+++ practice/mail.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask_mail import Mail, Message
+import config
 
 app = Flask(__name__)
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'quayeadelaide18@gmail.com'
-app.config['MAIL_PASSWORD'] = 'gksv gvgh duiv ipbq'
-app.config['MAIL_DEFAULT_SENDER'] = 'quayeadelaide18@gmail.com'
+
+app.config.update(config.EMAIL)
+
 
 mail = Mail(app)
 
